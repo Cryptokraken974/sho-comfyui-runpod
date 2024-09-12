160c46f9c84347bc1c626966b18ec6c982a9ac21
Cryptokraken974 <123509110+Cryptokraken974@users.noreply.github.com>
2024-09-12 08:30:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "pod started"
+echo "$$$ sho-comfyui-runpod@start.sh started"
 
 if [[ $PUBLIC_KEY ]]
 then
@@ -15,8 +15,12 @@ fi
 
 if [[ $JUPYTER_PASSWORD ]]
 then
+    echo "$$$ sho-comfyui-runpod@start.sh: Starting Jupyterlab"
     cd /
-    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace &
 else
     sleep infinity
 fi
+
+echo "$$$ sho-comfyui-runpod@start.sh: Starting ComfyUI"
+python3 /comfyui/main.py &
