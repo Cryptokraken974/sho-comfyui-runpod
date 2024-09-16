15166a0f8924e6a1161a58489bc7a6bfc9e680af
Cryptokraken974 <frinmuc@gmail.com>
2024-09-16 18:19:55 +0200

    good

--- dockerfile
+++ dockerfile
@@ -64,6 +64,9 @@ EXPOSE 22
 RUN git clone https://github.com/ltdrdata/ComfyUI-Manager ./ComfyUI/custom_nodes/ComfyUI-Manager && \
   pip3 install -r ./ComfyUI/custom_nodes/ComfyUI-Manager/requirements.txt
 
+RUN git clone https://github.com/cubiq/ComfyUI_essentials ./ComfyUI/custom_nodes/ComfyUI_essentials && \
+  pip3 install -r ./ComfyUI/custom_nodes/ComfyUI_essentials/requirements.txt
+
 RUN git clone https://github.com/ltdrdata/ComfyUI-Impact-Pack ./ComfyUI/custom_nodes/ComfyUI-Impact-Pack && \
   pip3 install -r ./ComfyUI/custom_nodes/ComfyUI-Impact-Pack/requirements.txt
 
@@ -78,6 +81,45 @@ RUN git clone https://github.com/storyicon/comfyui_segment_anything ./ComfyUI/cu
 
 RUN git clone --recursive https://github.com/receyuki/comfyui-prompt-reader-node ./ComfyUI/custom_nodes/comfyui-prompt-reader-node && \
   pip3 install -r ./ComfyUI/custom_nodes/comfyui-prompt-reader-node/requirements.txt
+
+RUN git clone https://github.com/melMass/comfy_mtb ./ComfyUI/custom_nodes/comfy_mtb && \
+  pip3 install -r ./ComfyUI/custom_nodes/comfy_mtb/requirements.txt
+
+RUN git clone https://github.com/giriss/comfy-image-saver ./ComfyUI/custom_nodes/comfy-image-saver && \
+  pip3 install -r ./ComfyUI/custom_nodes/comfy-image-saver/requirements.txt
+
+RUN git clone https://github.com/talesofai/comfyui-browser ./ComfyUI/custom_nodes/comfyui-browser && \
+  pip3 install -r ./ComfyUI/custom_nodes/comfyui-browser/requirements.txt
+
+RUN git clone https://github.com/ltdrdata/ComfyUI-Inspire-Pack ./ComfyUI/custom_nodes/ComfyUI-Inspire-Pack && \
+  pip3 install -r ./ComfyUI/custom_nodes/ComfyUI-Inspire-Pack/requirements.txt
+
+ RUN git clone https://github.com/kijai/ComfyUI-KJNodes ./ComfyUI/custom_nodes/ComfyUI-KJNodes && \
+  pip3 install -r ./ComfyUI/custom_nodes/ComfyUI-KJNodes/requirements.txt
+
+RUN git clone https://github.com/Gourieff/comfyui-reactor-node ./ComfyUI/custom_nodes/comfyui-reactor-node && \
+  pip3 install -r ./ComfyUI/custom_nodes/comfyui-reactor-node/requirements.txt
+
+RUN git clone https://github.com/jags111/efficiency-nodes-comfyui ./ComfyUI/custom_nodes/efficiency-nodes-comfyui && \
+  pip3 install -r ./ComfyUI/custom_nodes/efficiency-nodes-comfyui/requirements.txt
+
+RUN git clone https://github.com/rgthree/rgthree-comfy ./ComfyUI/custom_nodes/rgthree-comfy && \
+  pip3 install -r ./ComfyUI/custom_nodes/rgthree-comfy/requirements.txt
+
+RUN git clone https://github.com/blib-la/blibla-comfyui-extensions ./ComfyUI/custom_nodes/blibla-comfyui-extensions
+RUN git clone https://github.com/chrisgoringe/cg-use-everywhere ./ComfyUI/custom_nodes/cg-use-everywhere
+RUN git clone https://github.com/BlenderNeko/ComfyUI_ADV_CLIP_emb ./ComfyUI/custom_nodes/ComfyUI_ADV_CLIP_emb
+RUN git clone https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes ./ComfyUI/custom_nodes/ComfyUI_Comfyroll_CustomNodes
+RUN git clone https://github.com/kinfolk0117/ComfyUI_GradientDeepShrink ./ComfyUI/custom_nodes/ComfyUI_GradientDeepShrink
+RUN git clone https://github.com/cubiq/ComfyUI_IPAdapter_plus ./ComfyUI/custom_nodes/ComfyUI_IPAdapter_plus
+RUN git clone https://github.com/JPS-GER/ComfyUI_JPS-Nodes ./ComfyUI/custom_nodes/ComfyUI_JPS
+RUN git clone https://github.com/TinyTerra/ComfyUI_tinyterraNodes ./ComfyUI/custom_nodes/ComfyUI_tinyterraNodes
+RUN git clone https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet ./ComfyUI/custom_nodes/ComfyUI-Advanced-ControlNet
+RUN git clone https://github.com/pythongosssss/ComfyUI-Custom-Scripts ./ComfyUI/custom_nodes/ComfyUI-Custom-Scripts
+RUN git clone https://github.com/theUpsider/ComfyUI-Logic ./ComfyUI/custom_nodes/ComfyUI-Logic
+RUN git clone https://github.com/Nuked88/ComfyUI-N-Sidebar ./ComfyUI/custom_nodes/ComfyUI-N-Sidebar
+RUN git clone https://github.com/shiimizu/ComfyUI-TiledDiffusion ./ComfyUI/custom_nodes/ComfyUI-TiledDiffusion
+RUN git clone https://github.com/jitcoder/lora-info ./ComfyUI/custom_nodes/lora-info
 
 
 #one line example
--- setup_volume.sh
+++ setup_volume.sh
@@ -26,33 +26,33 @@ done
 # Clone custom nodes
 NODES=(
     #"https://github.com/ltdrdata/ComfyUI-Manager"
-    "https://github.com/cubiq/ComfyUI_essentials"
-    "https://github.com/chrisgoringe/cg-use-everywhere"
-    "https://github.com/melMass/comfy_mtb"
-    "https://github.com/giriss/comfy-image-saver"
-    "https://github.com/BlenderNeko/ComfyUI_ADV_CLIP_emb"
-    "https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
+    #"https://github.com/cubiq/ComfyUI_essentials"
+    #"https://github.com/chrisgoringe/cg-use-everywhere"
+    #"https://github.com/melMass/comfy_mtb"
+    #"https://github.com/giriss/comfy-image-saver"
+    #"https://github.com/BlenderNeko/ComfyUI_ADV_CLIP_emb"
+    #"https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes"
     #"https://github.com/Fannovel16/comfyui_controlnet_aux"
-    "https://github.com/kinfolk0117/ComfyUI_GradientDeepShrink"
-    "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
-    "https://github.com/JPS-GER/ComfyUI_JPS-Nodes"
+    #"https://github.com/kinfolk0117/ComfyUI_GradientDeepShrink"
+    #"https://github.com/cubiq/ComfyUI_IPAdapter_plus"
+    #"https://github.com/JPS-GER/ComfyUI_JPS-Nodes"
     #"https://github.com/storyicon/comfyui_segment_anything"
-    "https://github.com/TinyTerra/ComfyUI_tinyterraNodes"
-    "https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet"
-    "https://github.com/talesofai/comfyui-browser"
-    "https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
+    #"https://github.com/TinyTerra/ComfyUI_tinyterraNodes"
+    #"https://github.com/Kosinkadink/ComfyUI-Advanced-ControlNet"
+    #"https://github.com/talesofai/comfyui-browser"
+    #"https://github.com/pythongosssss/ComfyUI-Custom-Scripts"
     #"https://github.com/ltdrdata/ComfyUI-Impact-Pack"
-    "https://github.com/ltdrdata/ComfyUI-Inspire-Pack"
-    "https://github.com/kijai/ComfyUI-KJNodes"
-    "https://github.com/theUpsider/ComfyUI-Logic"
-    "https://github.com/Nuked88/ComfyUI-N-Sidebar"
+    #"https://github.com/ltdrdata/ComfyUI-Inspire-Pack"
+    #"https://github.com/kijai/ComfyUI-KJNodes"
+    #"https://github.com/theUpsider/ComfyUI-Logic"
+    #"https://github.com/Nuked88/ComfyUI-N-Sidebar"
     #"https://github.com/receyuki/comfyui-prompt-reader-node"
-    "https://github.com/Gourieff/comfyui-reactor-node"
-    "https://github.com/shiimizu/ComfyUI-TiledDiffusion"
-    "https://github.com/jags111/efficiency-nodes-comfyui"
-    "https://github.com/blib-la/blibla-comfyui-extensions"
-    "https://github.com/jitcoder/lora-info"
-    "https://github.com/rgthree/rgthree-comfy"
+    #"https://github.com/Gourieff/comfyui-reactor-node"
+    #"https://github.com/shiimizu/ComfyUI-TiledDiffusion"
+    #"https://github.com/jags111/efficiency-nodes-comfyui"
+    #"https://github.com/blib-la/blibla-comfyui-extensions"
+    #"https://github.com/jitcoder/lora-info"
+    #"https://github.com/rgthree/rgthree-comfy"
     #"https://github.com/WASasquatch/was-node-suite-comfyui"
 )
 
@@ -90,6 +90,8 @@ CLIP_MODELS=(
 
 VAE_MODELS=(
     "https://huggingface.co/black-forest-labs/FLUX.1-schnell/resolve/main/ae.safetensors"
+    "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
+    "https://huggingface.co/madebyollin/sdxl-vae-fp16-fix/resolve/main/sdxl_vae.safetensors"
 )
 
 for model in "${UNET_MODELS[@]}"; do
@@ -107,25 +109,49 @@ done
 # Additional model categories (commented out by default)
 # Uncomment and add URLs as needed
 
-# CHECKPOINT_MODELS=()
-# LORA_MODELS=()
-# ESRGAN_MODELS=()
+#civitai key: baa885d09b6692dad706f82d61a62a13
+
+CHECKPOINT_MODELS=(
+    #SDXL1.0
+    "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
+    #DucHaiten-Pony-Real PONY
+    "https://civitai.com/api/download/models/695106?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=baa885d09b6692dad706f82d61a62a13"
+    #Hassaku XL (sfw&nsfw) SDXL
+    "https://civitai.com/api/download/models/378499?type=Model&format=SafeTensor&size=pruned&fp=bf16&token=baa885d09b6692dad706f82d61a62a13"
+    #KarenDiffusion_V1 [NSFW] [XL]
+    "https://civitai.com/api/download/models/784834?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=baa885d09b6692dad706f82d61a62a13"
+    #DonutsDeliveryMix XL V2 - NSFW Photorealism
+    "https://civitai.com/api/download/models/743355?type=Model&format=SafeTensor&size=pruned&fp=fp16&token=baa885d09b6692dad706f82d61a62a13"
+)
+
+LORA_MODELS=(
+    #FLUX NSFW unlock
+    "https://civitai.com/api/download/models/780667?type=Model&format=SafeTensor&token=baa885d09b6692dad706f82d61a62a13"
+
+)
+
+ESRGAN_MODELS=(
+    #4x NMKD Superscale
+    "https://civitai.com/api/download/models/156841?type=Model&format=PickleTensor&token=baa885d09b6692dad706f82d61a62a13"
+    #
+)
+
 # CONTROLNET_MODELS=()
 
-# for model in "${CHECKPOINT_MODELS[@]}"; do
-#     download_model "$model" "comfyui_network/models/checkpoints"
-# done
+for model in "${CHECKPOINT_MODELS[@]}"; do
+    download_model "$model" "AIDOCS/models/checkpoints"
+done
 
-# for model in "${LORA_MODELS[@]}"; do
-#     download_model "$model" "comfyui_network/models/loras"
-# done
+for model in "${LORA_MODELS[@]}"; do
+    download_model "$model" "AIDOCS/models/loras"
+ done
 
-# for model in "${ESRGAN_MODELS[@]}"; do
-#     download_model "$model" "comfyui_network/models/upscale_models"
-# done
+for model in "${ESRGAN_MODELS[@]}"; do
+    download_model "$model" "AIDOCS/models/upscale_models"
+done
 
 # for model in "${CONTROLNET_MODELS[@]}"; do
-#     download_model "$model" "comfyui_network/models/controlnet"
+#     download_model "$model" "AIDOCS/models/controlnet"
 # done
 
 echo "Volume setup completed successfully!"
